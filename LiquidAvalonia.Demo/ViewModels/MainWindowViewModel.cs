dfbe52f9073a995530b8ac761bc5080b15fe41c5
Alexandr Pidanov <alexandrpi345@gmail.com>
2025-07-17 23:02:42 +0300

    Update demo app.

--- LiquidAvalonia.Demo/ViewModels/MainWindowViewModel.cs
+++ LiquidAvalonia.Demo/ViewModels/MainWindowViewModel.cs
@@ -9,6 +9,13 @@ namespace LiquidAvalonia.Demo.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    [Reactive]
+    public string Text { get; set; } = 
+        """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra lobortis est. Sed quis iaculis sem. Sed sit amet ipsum nisl. Sed eleifend imperdiet consectetur. Suspendisse potenti. Quisque vel ex orci. Duis mattis, orci et feugiat tristique, massa metus elementum lectus, vel sollicitudin ex dolor vitae diam. Vivamus orci ex, lobortis eget ipsum at, accumsan vestibulum leo. Sed finibus dictum eros, a commodo nunc congue vel. Aenean semper tortor ac tincidunt commodo. Suspendisse at tristique ligula. Integer in ornare augue, sit amet vulputate arcu. Nulla blandit mauris vel fermentum blandit. Quisque at aliquet turpis. Nunc ullamcorper sapien at est viverra feugiat.
+        Sed condimentum lectus libero, sit amet scelerisque est faucibus tincidunt. Pellentesque scelerisque orci at orci convallis luctus. Donec et iaculis nisl. Phasellus ornare maximus augue eget tincidunt. Fusce iaculis auctor nisi id rutrum. Nulla id sem magna. Nullam non velit ut ante venenatis ultricies. Nullam dictum mi quis leo fermentum, sit amet molestie dui vehicula. Nunc consequat mi quis velit malesuada, ac malesuada magna pellentesque. Praesent tristique massa sem, a euismod dui commodo quis. Donec suscipit imperdiet odio ut aliquet. Proin bibendum lacus nec bibendum gravida. Mauris tristique suscipit aliquam. Phasellus vel lorem auctor, aliquam risus at, posuere risus. 
+        """;
+    
     public MainWindowViewModel()
     {
         
--- LiquidAvalonia.Demo/Views/MainWindow.axaml.cs
+++ LiquidAvalonia.Demo/Views/MainWindow.axaml.cs
@@ -15,35 +15,35 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 
         ViewModel = new MainWindowViewModel();
         
-        // GlassPanel.PointerPressed += (sender, e) =>
-        // {
-        //     if (sender is not Glass _) return;
-        //
-        //     //e.Pointer.Capture(Glass);
-        //     _isPointerPressed = true;
-        // };
-        //
-        // GlassPanel.PointerReleased += (sender, e) =>
-        // {
-        //     if (sender is not Glass _) return;
-        //     
-        //     //e.Pointer.Capture(null);
-        //     _isPointerPressed = false;
-        // };
-        //
-        // PointerMoved += (sender, e) =>
-        // {
-        //     if (!_isPointerPressed) return;
-        //     
-        //     var mousePosWindow = e.GetPosition(this);
-        //     var glassPanelPosition = GlassPanel.Bounds.Position;
-        //     
-        //     var offsetX = mousePosWindow.X - glassPanelPosition.X;
-        //     var offsetY = mousePosWindow.Y - glassPanelPosition.Y;
-        //     
-        //     GlassPanel.RenderTransform = new TranslateTransform(
-        //         offsetX,
-        //         offsetY);
-        // };
+        GlassPanel.PointerPressed += (sender, e) =>
+        {
+            if (sender is not Glass _) return;
+        
+            //e.Pointer.Capture(Glass);
+            _isPointerPressed = true;
+        };
+        
+        GlassPanel.PointerReleased += (sender, e) =>
+        {
+            if (sender is not Glass _) return;
+            
+            //e.Pointer.Capture(null);
+            _isPointerPressed = false;
+        };
+        
+        PointerMoved += (sender, e) =>
+        {
+            if (!_isPointerPressed) return;
+            
+            var mousePosWindow = e.GetPosition(SquirclePanel);
+            var glassPanelPosition = GlassPanel.Bounds.Position;
+            
+            var offsetX = mousePosWindow.X - glassPanelPosition.X;
+            var offsetY = mousePosWindow.Y - glassPanelPosition.Y;
+            
+            GlassPanel.RenderTransform = new TranslateTransform(
+                offsetX,
+                offsetY);
+        };
     }
 }
